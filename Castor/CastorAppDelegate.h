3c32e373a560a12128d06e4c6add7dcb9ca3727c
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2011-04-29 19:47:13 +0900

    Initial Commit

---
+++ Castor/CastorAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  CastorAppDelegate.h
+//  Castor
+//
+//  Created by Nobuyuki Matsui on 11/04/29.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CastorAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ Castor/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Castor
+//
+//  Created by Nobuyuki Matsui on 11/04/29.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
